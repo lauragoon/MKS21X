21e2611e469bbbf23a60caac37c9ead1c4ad91cc
Laura Goon <laura.gooon@gmail.com>
2016-12-02 03:36:56 -0500

    driver file and more fxns

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,3 +1,29 @@
-public class SuperArrayIterator {
+import java.util.*;
+import java.lang.UnsupportedOperationException;
 
+public class SuperArrayIterator implements Iterator<String>{
+    private int current;
+    private int end;
+    
+    public SuperArrayIterator(int start, int stop){
+	current = start;
+	end = stop;
+    }
+    
+    public boolean hasNext(){
+	return current < end;
+    }
+
+    public int next(){
+	if (hasNext()){
+	    current++;
+	    return current-1;
+	} else {
+	    throw new NoSuchElementException();
+	}
+    }
+
+    public void remove(){
+	throw new UnsupportedOperationError();
+    }
 }
