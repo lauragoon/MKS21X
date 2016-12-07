15796d250f5f93f48e15aa2aa61fdec10e6c54d9
Laura Goon <laura.gooon@gmail.com>
2016-12-07 16:49:55 -0500

    initial commit

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -24,8 +24,7 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     // postcondition: creates a copy of a bar code
-    public Barcode clone(Barcode x){
-    }
+    // public Barcode clone(Barcode x){}
 
     // postcondition: computes and returns the check sum for _zip
     private int checkSum(){
@@ -36,10 +35,50 @@ public class Barcode implements Comparable<Barcode>{
 	return sum;
     }
 
+    // helper function!
+    // postcondition: convert num string to bar string
+    public String convert(String num){
+	String ret = "";
+	switch (num){
+	case "1": ret = ":::||";
+	case "2": ret = "::||:|";
+	case "3": ret = "::||:";
+	case "4": ret = ":|::|";
+	case "5": ret = ":|:|:";
+	case "6": ret = ":||::";
+	case "7": ret = "|:::|";
+	case "8": ret = "|::|:";
+	case "9": ret = "|:|::";
+	case "0": ret = "||:::";
+	default: break;
+	}
+	return ret;
+    }
+
     // postcondition: format zip + check digit + Barcode
     // ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"
-    public String toString(){}
+    public String toString(){
+	String code = "";
+	String digit = Integer.toString(checkSum()%10);
+	String zip_ = _zip + digit;
+	for (int i = 0; i < zip_.length(); i++){
+	    code += convert(zip_.substring(i,i+1));
+	    //code += zip_.substring(i,i+1);
+	}
+	return zip_ + "  " + "|" + code + "|";
+    }
 
     // postcondition: compares the zip + checkdigit, in numerical order
-    public int compareTo(Barcode other){}
+    public int compareTo(Barcode other){
+	return 1+1;
+    }
+
+    // main
+    public static void main (String[] args){
+	Barcode ex = new Barcode("11229");
+	System.out.println(ex.toString());
+	//System.out.println("TEST");
+	//System.out.println("digit: " + Integer.toString((1+1+2+2+9)%10));
+	System.out.println("|:::||:::||::|:|::|:||:|:::|:|:|");
+    }
 }
