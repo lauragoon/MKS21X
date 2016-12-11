3811cbc8117ca1f79ff3f85b9e6507e759a57091
Laura Goon <laura.gooon@gmail.com>
2016-12-10 21:47:39 -0500

    updated fxns, need to add error checks to toZip

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,36 +1,25 @@
 import java.lang.IllegalArgumentException;
 import java.lang.NumberFormatException;
+import java.util.Arrays;
 
 public class Barcode implements Comparable<Barcode>{
-  // instance variables
   private String _zip;
   private int _checkDigit;
+  private static final String[] codes = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
 
-  // constructors
-  // precondition: _zip.length() = 5 and zip contains only digits
-  // postcondition: throws a runtime exception zip is not the correct
-  //                or zip contains a non digit
-  //                _zip and _checkDigit are initialized
   public Barcode(String zip){
     if (zip.length() != 5){
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException("Invalid length");
     }
     try{
       Integer.parseInt(zip);
-    } catch (NumberFormatException e){
-      throw new IllegalArgumentException();
+    } catch(NumberFormatException e){
+      throw new IllegalArgumentException("Invalid character(s)");
     }
     _zip = zip;
-    _checkDigit = checkSum();
+    _checkDigit = checkSum()%10;
   }
 
-  // postcondition: creates a copy of a bar code
-  public Barcode clone(Barcode x){
-    Barcode cp = new Barcode(_zip);
-    return cp;
-  }
-
-  // postcondition: computes and returns the check sum for _zip
   private int checkSum(){
     int sum = 0;
     for (int i = 0; i < _zip.length(); i++) {
@@ -39,63 +28,53 @@ public class Barcode implements Comparable<Barcode>{
     return sum;
   }
 
-  // helper function!
-  // postcondition: convert num string to bar string
-  public String convert(String num){
-    String ret = "";
-    switch (num){
-      case "1":
-        ret = ":::||";
-        break;
-      case "2":
-        ret = "::|:|";
-        break;
-      case "3":
-        ret = "::||:";
-        break;
-      case "4":
-        ret = ":|::|";
-        break;
-      case "5":
-        ret = ":|:|:";
-        break;
-      case "6":
-        ret = ":||::";
-        break;
-      case "7":
-        ret = "|:::|";
-        break;
-      case "8":
-        ret = "|::|:";
-        break;
-      case "9":
-        ret = "|:|::";
-        break;
-      case "0":
-        ret = "||:::";
-        break;
-      default:
-        break;
+  public String toCode(String zip){
+    if (zip.length() != 5){
+      throw new IllegalArgumentException("Invalid length");
     }
+    try{
+      Integer.parseInt(zip);
+    } catch(NumberFormatException e){
+      throw new IllegalArgumentException("Invalid character(s)");
+    }
+    String ret = "|";
+    for (int i = 0; i < zip.length(); i++){
+      ret += codes[Character.getNumericValue(zip.charAt(i))];
+    }
+    ret += "|";
     return ret;
   }
 
-  // postcondition: format zip + check digit + Barcode
-  // ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"
-  public String toString(){
-    String code = "";
-    String digit = Integer.toString(checkSum()%10);
-    String zip_ = _zip + digit;
-    for (int i = 0; i < zip_.length(); i++){
-      //code += zip_.substring(i,i+1);
-      code += convert(zip_.substring(i,i+1));
+  public String toZip(String bar){
+    if (bar.length() != 32){
+      throw new IllegalArgumentException("Invalid length");
     }
-    return zip_ + "  " + "|" + code + "|";
+    if (bar.charAt(0) != '|' || bar.charAt(31) != '|'){
+      throw new IllegalArgumentException("Invalid end bars");
+    }
+    for (int i = 0; i < bar.length(); i++) {
+      if (bar.charAt(i) != ':' || bar.charAt(i) != '|'){
+        throw new IllegalArgumentException("Invalid character(s)");
+      }
+    }
+    String ret = "";
+    for (int i = 0; i < 27; i++){
+      ret += Integer.toString(Arrays.asList(codes).indexOf(bar.substring(i,i+5)));
+    }
+    
+    return ret;
   }
 
-  // postcondition: compares the zip + checkdigit, in numerical order
+  public String toString(){
+    String digit = Integer.toString(_checkDigit);
+    String zip_ = _zip + digit;
+    String code = toCode(zip_);
+    return zip_ + " " + code;
+  }
+
   public int compareTo(Barcode other){
     return (_zip + Integer.toString(_checkDigit)).compareTo(other._zip + Integer.toString(other._checkDigit));
   }
+
 
 }
